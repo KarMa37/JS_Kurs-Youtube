5566642fe3e4ab7766db47f24475236e1175a1ac
Marcin Karwacki <kreatywny20@wp.pl>
2018-03-06 23:05:22 +0100

    WHILE

--- test.js
+++ test.js
@@ -1,6 +1,8 @@
-var myAge = 30;
-if (myAge < 18 || myAge > 30) {
-    document.write("You aint coming!");
-} else {
-    document.write("You can come!");
+var age = 5;
+
+while (age < 10) {
+    console.log("Your age is less than 10");
+    age++;
 }
+
+document.write("You are now over 10");
