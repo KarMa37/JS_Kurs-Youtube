9f22b5c2d98631751088a66f87264ad20aa5c214
Marcin Karwacki <kreatywny20@wp.pl>
2018-03-07 22:57:16 +0100

    Array

--- test.js
+++ test.js
@@ -1,18 +1,35 @@
-var str = "hello, world";
-console.log(str);
+var myArray = [];
 
-var str2 = str.slice(2,9);
+console.log(myArray);
 
-console.log(str2);
+myArray[0] = 25;
+console.log(myArray[0]);
 
-var str3 = str.slice(2);
+myArray[1] = 35;
+console.log(myArray[1]);
 
-console.log(str3);
+myArray[2] = true;
+console.log(myArray[2]);
 
-var tags = "meat,ham,salami,pork,beaf,chicken";
+myArray[3] = "hello";
+console.log(myArray[3]);
 
-console.log(tags);
+console.log(myArray);
 
-var tagsArray = tags.split(",");
+myArray[2] = false;
+console.log(myArray[2]);
 
-console.log(tagsArray);
+console.log(myArray);
+
+var myArray2 = [20,10,"hi",false];
+console.log(myArray2);
+
+var myArray3 = new Array();
+console.log(myArray3);
+
+var myArray4 = new Array(5);
+console.log(myArray4);
+
+console.log(myArray2.length);
+console.log(myArray2.sort());
+console.log(myArray2.reverse());
