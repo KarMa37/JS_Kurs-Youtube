fcf8999da8a40b3a20fda4b303e38b7b9b1881de
Marcin Karwacki <kreatywny20@wp.pl>
2018-03-07 22:32:50 +0100

    String

--- test.js
+++ test.js
@@ -1,8 +1,40 @@
-var a = "a";
-var b = 5;
+var myString = "I'm a 'super' string";
 
-if (!isNaN(a)) {
-    console.log("Meaning of life is " + (a * b));
+console.log(myString);
+
+console.log(myString.length);
+
+console.log(myString.toUpperCase());
+console.log(myString.toLowerCase());
+
+console.log(myString.indexOf("string"));
+console.log(myString.indexOf("super"));
+
+if (myString.indexOf("super") === -1) {
+    console.log("It isn't in the string");
 } else {
-    console.log("That aint even a number");
+    console.log("It start at position "
+        + myString.indexOf("super"));
 }
+
+var string1 = "abc";
+var string2 = "bcd";
+
+console.log(string1 === string2);
+console.log(string1 < string2);
+
+var string3 = "abc";
+var string4 = "abc";
+
+console.log(string3 === string4);
+
+var string5 = "abc";
+var string6 = "ABC";
+
+console.log(string5 === string6);
+console.log(string5 < string6);
+
+var string7 = "abc";
+var string8 = "ABC";
+
+console.log(string7 === string8.toLowerCase());
