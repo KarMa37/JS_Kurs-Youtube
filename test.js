c030fd3454e5fa0ee6af436404f6d3e8aec89f8e
Marcin Karwacki <kreatywny20@wp.pl>
2018-03-07 19:36:41 +0100

    Continue & break

--- test.js
+++ test.js
@@ -1,7 +1,11 @@
-var links = document.getElementsByTagName("a");
+for (i = 0; i < 10; i++) {
+    if (i === 5 || i === 3) {
+        continue;
+    }
+    console.log(i);
 
-for (i = 1; i <= links.length; i++) {
-    console.log("This is a link number " + i);
+    if(i === 7) {
+        break;
+    }
 }
-
-document.write("All links now looped");
+console.log("I have broken out of the loop");
