fb0d06c1684274cb53a285832ff86de8b356f688
Marcin Karwacki <kreatywny20@wp.pl>
2018-03-07 21:57:33 +0100

    Numbers + typeof

--- test.js
+++ test.js
@@ -1,8 +1,13 @@
-function getAverage (a,b) {
-    var average = (a + b) / 2;
-    console.log(average);
-    return average;
-}
+var a = 5;
+var b = "5";
 
-var myResult = getAverage(7,11);
-console.log("The average is " + myResult);
+console.log(a+b);
+console.log(typeof (a + b));
+
+console.log(Math.ceil(5.01));
+console.log(Math.round(7.49));
+console.log(Math.floor(8.8));
+
+console.log(Math.max(7,4,9,8));
+
+console.log(Math.PI);
