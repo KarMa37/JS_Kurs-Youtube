bd214ccb2511bfcdf98a5ca55e4fdc5d492b066a
Marcin Karwacki <kreatywny20@wp.pl>
2018-03-23 22:59:40 +0100

    Form validation

--- test.js
+++ test.js
@@ -1,7 +1,12 @@
-myForm.name.onfocus = function(){
-    myForm.name.style.border = "4px solid pink";
-};
+var myForm = document.forms.myForm;
+var message = document.getElementById("message");
 
-myForm.name.onblur = function(){
-    myForm.name.style.border = "none";
-};
+myForm.onsubmit = function(){
+    if(myForm.name.value == ""){
+        message.innerHTML = "please enter a name";
+        return false;
+    } else{
+        message.innerHTML = "";
+        return true;
+    }
+}
